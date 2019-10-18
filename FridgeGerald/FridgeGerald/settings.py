5ec3a8c654565f3658bbadfb53d760b0c9ab1cbc
David Jašek <davijasek@gmail.com>
2019-10-18 23:42:29 +0100

    navbar

--- FridgeGerald/FridgeGerald/settings.py
+++ FridgeGerald/FridgeGerald/settings.py
@@ -16,6 +16,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
