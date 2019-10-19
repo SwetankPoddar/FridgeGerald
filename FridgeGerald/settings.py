e55fd85f49af16f868988e0f7c3532d27b1b29ed
David Jašek <davijasek@gmail.com>
2019-10-19 19:02:47 +0100

    heroku update 3

--- FridgeGerald/settings.py
+++ FridgeGerald/settings.py
@@ -126,4 +126,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATIC_URL = '/static/'
