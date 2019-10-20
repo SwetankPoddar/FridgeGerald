45087fc242043c00804c0f67b8a96958947c2b55
David Jašek <davijasek@gmail.com>
2019-10-20 11:24:04 +0100

    ssl update 3

--- FridgeGerald/settings.py
+++ FridgeGerald/settings.py
@@ -50,8 +50,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
