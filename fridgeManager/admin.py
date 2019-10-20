c8608540abdf9b9cf77f07f88fc4c06159f2b554
Swetank Poddar <2424088p@student.gla.ac.uk>
2019-10-20 13:43:52 +0100

    Linked userprofile

--- fridgeManager/admin.py
+++ fridgeManager/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from fridgeManager.models import FridgeFoodItem, Fridge, food_category, food_item
+from fridgeManager.models import FridgeFoodItem, Fridge, food_category, food_item, UserProfile
 
 admin.site.register(Fridge)
-
+admin.site.register(UserProfile)
 admin.site.register(FridgeFoodItem)
 
 admin.site.register(food_category)
